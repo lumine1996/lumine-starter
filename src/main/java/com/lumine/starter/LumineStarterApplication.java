4e13f506cbeff8e6446777a3efa8ec6e00758c12
cgm <675056544@qq.com>
2022-06-11 23:12:59 +0800

    [FEAT]添加Spring初始化内容，添加若干依赖，调整gitignore

---
+++ src/main/java/com/lumine/starter/LumineStarterApplication.java
@@ -0,0 +1,13 @@
+package com.lumine.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumineStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumineStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lumine/starter/LumineStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lumine.starter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class LumineStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+		log.debug("contextLoads.");
+	}
+
+}
